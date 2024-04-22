52a71bf866ae1257587051d02ae786dd3e20ab9e
Mohamed Ahmed <ma07041705@gmail.com>
2024-04-22 11:42:39 +0300

    Organizing Code with Import/Export Lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+// import {username , city }from "../data/user"
+
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {/* {username} is a Web Developer from {city} */}
       </h1>
     </div>
   );
 }
+
+export default Home;
